e3c99382400a143299921500a9ffe5a2a2f8e21b
heohyeri <12hsri@naver.com>
2026-02-09 13:25:57 +0900

    first commit

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from setuptools import setup
-import sys
+from setuptools import setup, find_packages
 
 setup(
     name='ir_sim',
-    py_modules=['ir_sim'],
+    packages=find_packages(),
     version= '2.0',
     install_requires=[
         'matplotlib',
